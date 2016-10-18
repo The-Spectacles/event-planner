f40586755c9465798a1be9276ce5c2cd28e48b6b
rebekahheacock <rebekah.heacock@gmail.com>
2016-10-18 19:37:22 -0400

    Use date and time formatting when displaying RSVPs
    Require the date & time formatting code in the `lib/` directory inside of `rsvps/ui.js`.
    
    Use it to format the dates and times when displaying RSVPs.

--- assets/scripts/rsvps/ui.js
+++ assets/scripts/rsvps/ui.js
@@ -1,6 +1,7 @@
 'use strict';
 
  // const app = require('../app');
+const formatDateTime = require('../../../lib/format-date-time');
 
 const myRsvpsTemplate = require('../templates/rsvps/my-rsvps.handlebars');
 const singleRsvpTemplate = require('../templates/rsvps/single-rsvp.handlebars');
@@ -11,11 +12,19 @@ const createRsvpSuccess = (data) => {
 
 const myRsvpsSuccess = (data) => {
   console.log("inside createRsvpSuccess", data);
+  data.rsvps = formatDateTime.formatDatesForDisplay(data.rsvps);
   $('.my-rsvps').html(myRsvpsTemplate(data));
 };
 
 const singleRsvpSuccess = (data) => {
   console.log('the single RSVP data is', data);
+  data.rsvp = formatDateTime.trimDateAndTime(data.rsvp);
+  if (data.rsvp.startTime) {
+    data.rsvp.startTime = formatDateTime.convertClock(data.rsvp.startTime);
+  }
+  if (data.rsvp.endTime) {
+    data.rsvp.endTime = formatDateTime.convertClock(data.rsvp.endTime);
+  }
   let rsvp = data.rsvp;
   $('.single-rsvp').html(singleRsvpTemplate(rsvp));
 };
