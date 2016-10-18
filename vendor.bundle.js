d79eab298d42810e5597ccc4fca0b725a23744d1
JaimeGaribaldo7 <jaimegaribaldo753@gmail.com>
2016-10-18 09:34:12 -0400

    add bundle and vendor for commit

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(41);
+	module.exports = __webpack_require__(54);
 
 
 /***/ },
@@ -9926,7 +9926,7 @@
 
 /***/ },
 
-/***/ 41:
+/***/ 54:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
